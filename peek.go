0f144be7d7714407da148929ef98b8c25020f5c9
Spenser Black <spenserblack01@gmail.com>
2026-06-16 16:57:09 -0400

    Turn references to `iter.Pull` into links

--- peek.go
+++ peek.go
@@ -2,10 +2,10 @@
 package peek
 
 // PullFunc is a function that returns the next value from a pull-style iterator. See the
-// documentation for iter.Pull for more information.
+// documentation for [iter.Pull] for more information.
 type PullFunc[V any] = func() (V, bool)
 
-// Peek takes next function (typically from iter.Pull) and returns a new next function
+// Peek takes next function (typically from [iter.Pull]) and returns a new next function
 // and a peek function. You can call peek as many times as you want without consuming the iterator.
 // Once next is called, the peeked item (if peek was called) is consumed.
 func Peek[V any](next PullFunc[V]) (pull PullFunc[V], peek PullFunc[V]) {
@@ -33,10 +33,10 @@ func Peek[V any](next PullFunc[V]) (pull PullFunc[V], peek PullFunc[V]) {
 }
 
 // PullFunc2 is a function that returns the next value from a pull-style iterator. See the
-// documentation for iter.Pull2 for more information.
+// documentation for [iter.Pull2] for more information.
 type PullFunc2[K any, V any] = func() (K, V, bool)
 
-// Peek2 takes next function (typically from iter.Pull) and returns a new next function
+// Peek2 takes next function (typically from [iter.Pull]) and returns a new next function
 // and a peek function. You can call peek as many times as you want without consuming the iterator.
 // Once next is called, the peeked item (if peek was called) is consumed.
 func Peek2[K any, V any](next PullFunc2[K, V]) (pull PullFunc2[K, V], peek PullFunc2[K, V]) {
